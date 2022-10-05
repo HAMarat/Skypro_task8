aa1eb5eb2c969440c2347ca5e81d3b4556f3e9af
HAMarat <smile56680@gmail.com>
2022-10-05 06:49:54 +0300

    6:50 05.10

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
 import json
-class Question():
 
-    def __init__(self, text_question, complexity, rigth_answer, get_question=False, person_answer=None):
-        self.text_question = text_question
-        self.complexity = complexity
-        self.rigth_answer = rigth_answer
-        self.get_question = get_question
-        self.person_answer = person_answer
-        self.points = self.complexity * 10
+#
+# class Question:
+#
+#     def __init__(self, question, complexity, right_answer, get_question=False, person_answer=None):
+#         self.question = question
+#         self.complexity = complexity
+#         self.right_answer = right_answer
+#         self.get_question = get_question
+#         self.person_answer = person_answer
+#         self.points = None
+#
+#     def get_points(self):
+#         """Возвращает int, количество баллов.
+#         Баллы зависят от сложности: за 1 дается 10 баллов, за 5 дается 50 баллов.
+#         """
+#         self.points = self.complexity * 10
+#         return self.points
+#
+#     def is_correct(self):
+#         """Возвращает True, если ответ пользователя совпадает
+#         с верным ответов иначе False.
+#         """
+#         return self.right_answer == self.person_answer
+#
+#     def build_question(self):
+#         """Возвращает вопрос в понятном пользователю виде, например:
+#         Вопрос: What do people often call American flag?
+#         Сложность 4/5
+#         """
+#
+#     def build_positive_feedback(self):
+#         """Возвращает :
+#         Ответ верный, получено __ баллов
+#         """
+#
+#     def build_negative_feedback(self):
+#         """Возвращает :
+#         Ответ неверный, верный ответ __
+#         """
 
-    def get_points(self):
-    """Возвращает int, количество баллов.
-    Баллы зависят от сложности: за 1 дается 10 баллов, за 5 дается 50 баллов.
-    """
-    def is_correct():
-    """Возвращает True, если ответ пользователя совпадает
-    с верным ответов иначе False.
-    """
-    def build_question():
-    """Возвращает вопрос в понятном пользователю виде, например:
-    Вопрос: What do people often call American flag?
-    Сложность 4/5
-    """
 
-    def build_positive_feedback():
-    """Возвращает :
-    Ответ верный, получено __ баллов
-    """
-    def build_negative_feedback():
-    """Возвращает :
-    Ответ неверный, верный ответ __
-    """
+with open('question.json', 'r', encoding='utf-8') as file:
+    json_data = json.load(file)
 
-
+for question_dict in json_data:
 
